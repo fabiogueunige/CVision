a511331cbb442b5b57e4f48f8875478def842190
Andrea Chiappe <chiappe.emiliani99@gmail.com>
2023-12-06 14:06:12 +0100

    add a findmatched.m finctons

--- EightPointsAlgorithm.m
+++ EightPointsAlgorithm.m
@@ -6,11 +6,13 @@ function [F] = EightPointsAlgorithm(P1, P2,row_number)
         a = P1(i,:) .* P2(:,i);
         A(i, :) = reshape(a, [1, 9]);        
     end
+
     % apply SDV decomposition to A mtrix
     [U, D, V] = svd(A);
     f = V(:, end);
     F = reshape(f, [3, 3]);
-
+    
+    % Force the rank of f to be 2
     [U, D, V] = svd(F)
     D(3,3) = 0;
     F = U*D*V';
