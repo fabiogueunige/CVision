db2df82b33d40ec2e46ebc174f24566620f95eaf
Andrea Chiappe <chiappe.emiliani99@gmail.com>
2023-12-06 10:22:24 +0100

    finished code, need to do point 3

---
+++ EightPointsAlgorithm.m
@@ -0,0 +1,17 @@
+function [F] = EightPointsAlgorithm(P1, P2,row_number)
+% Remember to pass P1 transpose
+
+    % write the A matrix
+    for i = 1:row_number
+        a = P1(i,:) .* P2(:,i);
+        A(i, :) = reshape(a, [1, 9]);        
+    end
+    % apply SDV decomposition to A mtrix
+    [U, D, V] = svd(A);
+    f = V(:, end);
+    F = reshape(f, [3, 3]);
+
+    [U, D, V] = svd(F)
+    D(3,3) = 0;
+    F = U*D*V';
+end
