c0356dd575290c5d941af460237fb5fa0e93b5a0
fabbio0 <fabio.guelfi64@gmail.com>
2023-12-06 14:08:02 +0100

    Mtico Andre pt 2

--- EightPointsAlgorithmN.m
+++ EightPointsAlgorithmN.m
@@ -2,7 +2,8 @@ function [F] = EightPointsAlgorithmN(P1, P2, row_number)
 
     [nP1, T1] = normalise2dpts(P1);
     [nP2, T2] = normalise2dpts(P2);
-
+    
+    % normalized founfamental matrix
     Z = EightPointsAlgorithm(nP1', nP2, row_number);
     F = (T2')*Z*(T1);
 
