4f5b62d07a4d3b47fb118102c9b48c76ddc11c26
fabbio0 <fabio.guelfi64@gmail.com>
2023-12-29 17:10:29 +0100

    Qui funziona tutto

--- Code/TwoFramesLK.m
+++ Code/TwoFramesLK.m
@@ -1,4 +1,4 @@
-function [] = TwoFramesLK(pathframe1, pathframe2, windowSize)
+function [U,V] = TwoFramesLK(pathframe1, pathframe2, windowSize)
 
     At = imread(pathframe1);
     At1 = imread(pathframe2);
@@ -11,18 +11,21 @@ function [] = TwoFramesLK(pathframe1, pathframe2, windowSize)
     
     [U,V] = LucasKanade(At, At1, windowSize);
     
-    figure(1)
+    figure
     subplot(1,2,1)
     imshow(At)
     subplot(1,2,2)
     imshow(At+1)
-    figure(2)
+    figure
     % SEE help quiver for more information
-    quiver(Uf(1:10:size(Uf,1), 1:10:size(Uf,2)), Vf(1:10:size(Vf,1), 1:10:size(Vf,2)))
+    quiver(U(1:10:size(U,1), 1:10:size(U,2)), V(1:10:size(V,1), 1:10:size(V,2)))
     % suggestion to use somethng bigger than 1
     
     %Uso quiver(u, -v, 0) per visualizzare il campo vettoriale (u,v)
     %
-    %quiver(Reduce((Reduce(medfilt2(flipud(U),[5 5])))), -Reduce((Reduce(medfilt2(flipud(V),[5 5])))), 0), axis equal
+    quiver(Reduce((Reduce(medfilt2(flipud(U),[5 5])))), -Reduce((Reduce(medfilt2(flipud(V),[5 5])))), 0), axis equal
+end
 
+function result = Reduce(matrix)
+    result = matrix(1:2:end, 1:2:end);
 end
---
+++ Code/main.m
@@ -0,0 +1,18 @@
+addpath("Video\")
+% ottengo percorso dove si trova il main
+
+
+stringa1 = 'Video/sphere/sphere.';
+stringa3 = '.ppm';
+
+for k = 0:18
+    string2 = num2str(k);
+    stringtot = strcat(stringa1, string2, stringa3);
+    string2_next = num2str((k + 1));
+    stringtot2 = strcat(stringa1, string2_next, stringa3);
+    [U,V] = TwoFramesLK(stringtot, stringtot2, 10);
+    disp(U);
+    disp(V);
+    pause(5);
+    %title(['Frames: ' num2str(k) ' - ' num2str(k + 1)]);
+end
